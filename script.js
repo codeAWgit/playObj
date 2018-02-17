d8bd2167545d137d9c8a049f7b1655e343a4850b
codeAWgit <36483111+codeAWgit@users.noreply.github.com>
2018-02-16 22:21:23 -0600

    lll

--- script.js
+++ script.js
@@ -4,46 +4,32 @@ class Fill{
         
         this.makeFive = ()=>{
             for(var i=this.smallArr.length; i<5; i++){
-                this.smallArr.push('')
+                this.smallArr.push(0)
             }
         }
         
         this.makeAllOneNum = (num) => {
-            this.smallArr = this.smallArr.map(()=>{
-                return num
-            })
+            this.smallArr = this.smallArr.fill(num)
         }
     }
 }
-
-let harray = new Array(1,3,5)
+let harray = new Array(3)
 arr = new Fill(harray)
 
-
-
 $(() => {
-    'use strict'
-    
     $('#output').text(JSON.stringify(arr))
     
     arr.makeFive()
-    
     $('#output').append('<div>[ ' + arr.smallArr + ' ]</div>') 
     
     arr.makeAllOneNum(1)
-    
     $('#output').append('<div>[ ' + arr.smallArr + ' ]</div>')
     
-//    $('button').on('click', () => {
-//        $('#output').css('display','none')
-//    })
     
     setTimeout(()=>{
-        alert( arr.smallArr['0'] ** 2 == 1 ? "hi Dan" : "bye Dan")
+        alert( arr.smallArr[0] ** 2 == 1 ? "hi Dan" : "bye Dan")
         arr.otherKey = "About done with " +
         "this timeout."                   //<--Preferred way to line 
         $('#output').append(arr.otherKey)   //-break a string in JS.
-    }, 5000)
-    
-
+    }, 3000)
 })
