c3df3a21f674d2496bbd3d24d1db96d116042a6d
codeAWgit <--global>
2018-02-17 09:46:45 -0600

    More play, this time with a few functions.

--- script.js
+++ script.js
@@ -16,6 +16,26 @@ class Fill{
 let harray = new Array(3)
 arr = new Fill(harray)
 
+/*
+function factorialize(num) {		 First way I learned to make this function.
+	var result = 1;
+	for( var i = 1; i <= num ; i++){
+		result *= i;
+	}
+	return result;
+} */
+/*
+function factorialize(num) {		 Second way I learned to make this function.
+	if (num == 1 || !num){
+      return 1}
+	return num * factorialize(num-1)
+} */
+				// Most recent way I've learned to make this function.
+
+function factorialize(num) {		
+	return (num == 1 || !num ? 1 : num * factorialize(num-1))
+}
+
 $(() => {
     $('#output').text(JSON.stringify(arr))
     
@@ -30,6 +50,7 @@ $(() => {
         alert( arr.smallArr[0] ** 2 == 1 ? "hi Dan" : "bye Dan")
         arr.otherKey = "About done with " +
         "this timeout."                   //<--Preferred way to line 
-        $('#output').append(arr.otherKey)   //-break a string in JS.
+        //$('#output').append(arr.otherKey)   //-break a string in JS.
+	$('#output').append(factorialize(5))
     }, 3000)
 })
