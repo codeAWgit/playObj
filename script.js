44b7193ea44c996a8acdd9eadc210e2b175edbf4
codeAWgit <36483111+codeAWgit@users.noreply.github.com>
2018-02-17 22:36:22 -0600

    lll

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class Fill{
     constructor(arr){
+        if(arr == undefined) {arr = []} // Good to initialize params.
         this.smallArr = arr
         
         this.makeFive = ()=>{
@@ -13,27 +14,30 @@ class Fill{
         }
     }
 }
-let harray = new Array(3)
-arr = new Fill(harray)
+let harray// = new Array(3)
+try {
+    arr = new Fill(harray)
+} catch (e) {
+    //Catch Statement
+}
 
-/*
-function factorialize(num) {		 First way I learned to make this function.
-	var result = 1;
-	for( var i = 1; i <= num ; i++){
+ /*                     First way I learned to make this function.
+function factorialize(num) {
+    let result = 1
+	for (let i = 1; i <= num ; i++){
 		result *= i;
 	}
 	return result;
-} */
-/*
-function factorialize(num) {		 Second way I learned to make this function.
+} 
+                     Second way I learned to make this function.
+function factorialize(num) {		 
 	if (num == 1 || !num){
       return 1}
 	return num * factorialize(num-1)
 } */
-				// Most recent way I've learned to make this function.
-
+                // Most recent way I've learned to make this function.
 function factorialize(num) {		
-	return (num == 1 || !num ? 1 : num * factorialize(num-1))
+	return (num == 1 || !num ? 1 : num * factorialize(num - 1))
 }
 
 $(() => {
@@ -45,12 +49,16 @@ $(() => {
     arr.makeAllOneNum(1)
     $('#output').append('<div>[ ' + arr.smallArr + ' ]</div>')
     
+    debugger
     
-    setTimeout(()=>{
+    let timeout = setTimeout(()=>{
         alert( arr.smallArr[0] ** 2 == 1 ? "hi Dan" : "bye Dan")
+        
+        debugger
+        
         arr.otherKey = "About done with " +
         "this timeout."                   //<--Preferred way to line 
         //$('#output').append(arr.otherKey)   //-break a string in JS.
-	$('#output').append(factorialize(5))
+	   $('#output').append(factorialize(8))
     }, 3000)
 })
