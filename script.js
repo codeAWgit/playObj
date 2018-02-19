4bffb2c04eb8c9ffba42adbee3f42767fad61ce5
codeAWgit <36483111+codeAWgit@users.noreply.github.com>
2018-02-19 15:49:09 -0600

    Test of function displayed in index file

--- script.js
+++ script.js
@@ -66,10 +66,9 @@ $(() => {
     //                         factorialize(10) + '</p>')
     // }, 2000)
 
-let findLongestWord = (str) => {
-    return Math.max.apply(null, str.split(' ').map(x => x.length));
+let largestOfFour = (arr) => {
+    return arr.map(x => Math.max.apply(null, x))
     }
-    
-let tmp = findLongestWord("The quick brown fox jumped over the lazy dog");
-$('#output').append(tmp)
+
+$('#output').append(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]))
 })
