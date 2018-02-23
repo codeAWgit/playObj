b8ae56a09a8807beb02a4ba71288d7533bb7256d
codeAWgit <36483111+codeAWgit@users.noreply.github.com>
2018-02-22 20:20:01 -0600

    Removed jQuery to use vanilla JS. Refactored much and cleaned the layout and look of the code.

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 'use strict';
 
+//#region class Fill constructor 
 class Fill {
-    constructor(arr) {
-        if (arr === undefined) {arr = []} // Good to initialize params.
-        this.smallArr = arr
-        
-        this.makeFive = () => {
-            for(var i= this.smallArr.length; i < 5; i++) {
+    constructor(arr) {                    // Region to initialize class objects.           
+        //if (arr === undefined) {arr = []}   Good to initialize params.
+        this.smallArr = arr || []         // This does the same as the previous extra line above.
+    }
+    makeFive() {                          // Region where methods exist and how their syntax looks.
+        for(var i = this.smallArr.length; i < 5; i++) {
                 this.smallArr.push(0)
             }
         }
         
-        this.makeAllOneNum = (num) => {
+    makeAllOneNum(num) {
             this.smallArr = this.smallArr.fill(num)
         }
     }
-}
+//#endregion
+
 let harray //= new Array(3)
 let arr
+
 try {
     arr = new Fill(harray)
 } catch (e) {
     //document.write(e.message)
 }
 
+//#region factorialize function
  /*                     First way I learned to make this function.
 function factorialize(num) {
     let result = 1
@@ -39,22 +43,30 @@ function factorialize(num) {
 	return num * factorialize(num-1)
 } */
                 // Most recent way I've learned to make this function.
-let factorialize = (num) => {		
-	return (num === 1 || num === 0 ? 1 : num * factorialize(num - 1))
-}
+// let factorialize = (num) => {		
+// 	return (num === 1 || num === 0 ? 1 : num * factorialize(num - 1))
+// }
+//#endregion
 
-$( () => {    
+//$( () => {                                 // Old jQuery stuff
+//})
+
+window.onload = function() {                // Main outputs for index.html
     let stringArr = JSON.stringify(arr)
-    $('#output').text(stringArr)
+    let outputDOM = document.getElementById('output')
+    outputDOM.innerHTML = stringArr 
+    //$('#output').text(stringArr)
     
     arr.makeFive()
-    $('#output').append('<div>[ ' + arr.smallArr + ' ]</div>') 
+    outputDOM.innerHTML += '<div>[ ' + arr.smallArr + ' ]</div>'
+    //$('#output').append('<div>[ ' + arr.smallArr + ' ]</div>') 
     
     arr.makeAllOneNum(1)
-    $('#output').append('<div>[ ' + arr.smallArr + ' ]</div>')
+    outputDOM.innerHTML += '<div>[ ' + arr.smallArr + ' ]</div>'
+    //$('#output').append('<div>[ ' + arr.smallArr + ' ]</div>')
+
     
-    //debugger
-    
+    //#region playing with setTimeout
     // let timeout = setTimeout(() => {
     //     alert( arr.smallArr[0] ** 2 == 1 ? 'hi Dan' : 'bye Dan')
         
@@ -63,57 +75,59 @@ $( () => {
     //     //$('#output').append(arr.otherKey)   
     //     $('#output').append('<br/><p>Five factorial is: ' + 
     //                         factorialize(5) + '</p>')
-    //     $('#output').append('<p>Ten factorial is: ' + 
-    //                         factorialize(10) + '</p>')
     // }, 2000)
+    //#endregion
 
-    //Example of a closure to access and increase a local variable in the self-invoked function.
-// let largestOfFour = (() => {
-//     let counter = 0
-//     return function () {
-//         return ++counter
-//     }
-// })()
+    //#region Closure example to access, increase a local variable in the self-invoked function.
+    let largestOfFour = ( () => {
+        let counter = 0
+        return function () {
+            return ++counter
+        }
+    })()
 
-// largestOfFour()
+    outputDOM.innerHTML += largestOfFour()
+    //#endregion
 
-// document.getElementById('output').innerHTML += largestOfFour()
+    //#region    Test code region 
+    // document.getElementById('output').innerHTML += largestOfFour()
 
-    //Example of using continue in a for loop to jump over certain numbers & keep them from 
-    //  adding to the text string to later output to the screen.
-// var text = "";
-// var i;
-// for (i = 1; i < 10; i++) {
-//     if (i === 5 || i === 7) continue
-//     text += i + "<br>"
-// }
+        //Example of using continue in a for loop to jump over certain numbers & keep them from 
+        //  adding to the text string to later output to the screen.
+    // var text = "";
+    // var i;
+    // for (i = 1; i < 10; i++) {
+    //     if (i === 5 || i === 7) continue
+    //     text += i + "<br>"
+    // }
 
-// document.getElementById("output").innerHTML += text
+    // document.getElementById("output").innerHTML += text
 
-// let person = {
-//     firstName:"John",
-//     lastName: "Doe",
-//     fullName: function() {
-//         return this.firstName + " " + this.lastName
-//     }
-// }
+    // let person = {
+    //     firstName:"John",
+    //     lastName: "Doe",
+    //     fullName: function() {
+    //         return this.firstName + " " + this.lastName
+    //     }
+    // }
 
-// let myObject = {
-//     firstName:"Mary",
-//     lastName: "Doe"
-// }
+    // let myObject = {
+    //     firstName:"Mary",
+    //     lastName: "Doe"
+    // }
 
-// document.getElementById("output").innerHTML += person.fullName.call(myObject)
+    // document.getElementById("output").innerHTML += person.fullName.call(myObject)
 
-let chunkArrayInGroups = (arr, size) => {
-    let newRay = []
+    // let chunkArrayInGroups = (arr, size) => {
+    //     let newRay = []
+        
+    //     while( arr.length ) { 
+    //         newRay.push(arr.splice(0,size))
+    //     }
+    //     return newRay
+    // }
     
-    while( arr.length ) { 
-        newRay.push(arr.splice(0,size))
-    }
-    return newRay
-}
-  
-console.error(chunkArrayInGroups(["a", "b", "c", "d", "e"], 2));
+    // console.error(chunkArrayInGroups(["a", "b", "c", "d", "e"], 2));
+    //#endregion
 
-})
+}
